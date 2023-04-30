ab1549cbc0d49aae0e05dd43d7c37a6870e07907
Yuliia Palamar <yuliyapalamar@gmail.com>
2023-04-30 23:21:57 +0300

    Add project files.

---
+++ cardgame/card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cardgame
+{
+    public class Card
+    {
+        public string Suit { get; set; }
+        public string Value { get; set; }
+
+        public Card(string suit, string value)
+        {
+            Suit = suit;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value} of {Suit}";
+        }
+    }
+}
