91d14957d797f4791530b6c38bb8db21e20e8a7a
katerynafomina <kf30766@gmail.com>
2023-05-23 10:48:19 +0300

    add gamemode

--- cardgame/Program.cs
+++ cardgame/Program.cs
@@ -5,18 +5,26 @@
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-
             Console.Write("Введіть кількість гравців: ");
             int numPlayers = int.Parse(Console.ReadLine());
-            if (numPlayers > 1 && numPlayers < 7)
-            {
-                Game game = new Game(numPlayers);
-                game.Start();
-            }
-            else
+            
+            if(numPlayers <2 && numPlayers>6)
             {
                 Console.WriteLine("Кількість гравців не може бути менша 2 і більша за 6");
             }
+                
+            Console.WriteLine("Виберіть режим гри:\n1-грати самому\n2-комп'ютерне моделювання");
+            int gamemode = int.Parse(Console.ReadLine());
+            switch(gamemode)
+            {
+                case 1:
+                    Game game = new Game(numPlayers);
+                    game.Start();
+                    break;
+                case 2:
+                    break;
+            }
+                     
         }
     }
 }
